e8bdd24f453372da10a048a834474371783a1efe
sheviv <shevchenkoival@gmail.com>
2021-02-22 02:57:55 +0300

    //

---
+++ matplotlib_1.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 5, 0.1)
+y = x ** 2
+plt.figure()
+plt.plot(x, y, "r")
+plt.xlabel("ыва")
+plt.ylabel("Y")
+plt.title("Title")
+plt.show()
+
+
