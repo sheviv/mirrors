3cd2ecdbd5b1e86389a8f4d296d9dd5564cc5e76
sheviv <shevchenkoival@gmail.com>
2021-12-29 19:51:31 +0300

    step_2

---
+++ cpp/comp_sc_center/w_1/ex_2.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+//"перевернуть" последовательность положительных целых чисел
+//Sample Input:
+//15 26 1 42 0
+//Sample Output:
+//42 1 26 15
+// void obr()
+// {
+//     int n;
+//     cin >> n;
+//     if (n)
+//     {
+//         obr();
+//         cout << n << '\t';
+//     }
+// }
+// int main()
+// {
+//     obr();
+//     return 0;
+// }
+
