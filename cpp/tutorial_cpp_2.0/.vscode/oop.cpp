e23bd60cbed1d1bfb262ce373c6ca0167b27218e
sheviv <shevchenkoival@gmail.com>
2022-01-27 11:44:56 +0300

    oop_2

--- cpp/tutorial_cpp_2.0/.vscode/oop.cpp
+++ cpp/tutorial_cpp_2.0/.vscode/oop.cpp
@@ -601,15 +601,74 @@
 
 //////////
 // Статические переменные-члены класса
-int generateID()
+// int generateID()
+// {
+//     static int s_id = 0;
+//     return ++s_id;
+// }
+// int main()
+// {
+//     std::cout << generateID() << '\n';  // 1
+//     std::cout << generateID() << '\n';  // 2
+//     std::cout << generateID() << '\n';  // 3
+//     return 0;
+// }
+
+
+
+////////
+// Дружественные функции и классы
+// class Anything
+// {
+//     private:
+//         int m_value;
+//     public:
+//         Anything() { m_value = 0; }
+//         void add(int value) { m_value += value; }
+//         // Делаем функцию reset() дружественной классу Anything
+//         friend void reset(Anything &anything);
+// };
+// // Функция reset() теперь является другом класса Anything
+// void reset(Anything &anything)
+// {
+//     // И мы имеем доступ к закрытым членам объектов класса Anything
+//     anything.m_value = 0;
+// }
+// int main()
+// {
+//     Anything one;
+//     one.add(4); // добавляем 4 к m_value
+//     reset(one); // сбрасываем m_value в 0
+//     return 0;
+// }
+
+///
+class Humidity;
+class Temperature
 {
-    static int s_id = 0;
-    return ++s_id;
+    private:
+        int m_temp;
+    public:
+        Temperature(int temp=0) { m_temp = temp; }
+        friend void outWeather(const Temperature &temperature, const Humidity &humidity);
+};
+class Humidity
+{
+    private:
+        int m_humidity;
+    public:
+        Humidity(int humidity=0) { m_humidity = humidity; }
+        friend void outWeather(const Temperature &temperature, const Humidity &humidity);
+};
+void outWeather(const Temperature &temperature, const Humidity &humidity)
+{
+    std::cout << "The temperature is " << temperature.m_temp <<
+    " and the humidity is " << humidity.m_humidity << '\n';
 }
 int main()
 {
-    std::cout << generateID() << '\n';  // 1
-    std::cout << generateID() << '\n';  // 2
-    std::cout << generateID() << '\n';  // 3
+    Temperature temp(15);
+    Humidity hum(11);
+    outWeather(temp, hum);
     return 0;
 }
